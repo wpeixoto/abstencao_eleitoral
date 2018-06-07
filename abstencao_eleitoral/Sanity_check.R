29e38e52458fd9a9b9ec40f01d3c6b00690aae92
William Peixoto <william@Tanis.nowhere>
2018-06-07 14:19:08 -0300

    Separação de nomes de arquivos e suas leituras
    Os nomes de arquivos agora são gerados e verificados por existência, todos de uma vez.
    
    Os arquivos existentes são lidos no loop seguinte.

--- abstencao_eleitoral/Sanity_check.R
+++ abstencao_eleitoral/Sanity_check.R
@@ -1,12 +1,17 @@
-UFs = c("AC", "AL", "AP", "AM", "BA", "CE", "DF", "ED", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "RJ", "PI", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO")
-# UFs = c("AC", "AL")  # , "AP", "AM", "BA", "CE", "DF", "ED", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "RJ", "PI", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO")
+UFs = c("AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", 
+        "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR",
+        "PE", "RJ", "PI", "RN", "RS", "RO", "RR", "SC", 
+        "SP", "SE", "TO")
+# UFs = c("AC", "AL")  # , "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "RJ", "PI", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO")
+ANOS = seq(1994,2016,2)
 
-get_dv = function(filename, all = FALSE, full=FALSE, cargo=3) {
+
+get_dv = function(filename, all = FALSE, full=FALSE, cargos= c(3, 9))  {
   dv = read.csv(filename, header = F, encoding = "ISO-8859", sep = ";")
   names(dv) = c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO", "NUM_TURNO", "DESCRICAO_ELEICAO", "SIGLA_UF", "SIGLA_UE", "CODIGO_MUNICIPIO", "NOME_MUNICIPIO", "NUMERO_ZONA", "NUMERO_SECAO", "CODIGO_CARGO", "DESCRICAO_CARGO", "QTD_APTOS", "QTD_COMPARECIMENTO", "QTD_ABSTENCOES", "QT_VOTOS_NOMINAIS", "QT_VOTOS_BRANCOS", "QT_VOTOS_NULOS", "QT_VOTOS_LEGENDA", "QT_VOTOS_ANULADOS_APU_SEP")
   
   if (!all) {
-    dv = dv[dv$CODIGO_CARGO == cargo, ]  # Restringir a Governador, que tem os dois turnos. TODO: O que fazer se não tiver tido 2º turno? Presidente quase sempre tem
+    dv = dv[dv$CODIGO_CARGO %in% cargos, ]  # Restringir a Governador e prefeito, que tem os dois turnos. TODO: O que fazer se não tiver tido 2º turno? Presidente quase sempre tem. Prefeito parece ser o contrário
   }
   
   
@@ -36,54 +41,78 @@ get_dv = function(filename, all = FALSE, full=FALSE, cargo=3) {
 # ===============================================================
 # AQUI
 # ===============================================================
-MaxSize = length(UFs) * 12
+
+MaxSize = length(UFs) * length(ANOS)
 rm(ttt)
 ttt = data.frame(
-  ano=rep(NA, MaxSize), 
-  uf=rep("", MaxSize), 
-  isna=rep(NA, MaxSize),
-  orig_rows=rep(NA, MaxSize),
-  n_rows_gov=rep(NA, MaxSize),
-  verif_taxa_consistente=rep(NA, MaxSize),
-  stringsAsFactors = F
-  )
+ ano=rep(NA, MaxSize), 
+ uf=rep("", MaxSize), 
+ any_na=rep(NA, MaxSize),
+ orig_rows=rep(NA, MaxSize),
+ n_rows_gov=rep(NA, MaxSize),
+ verif_taxa_consistente=rep(NA, MaxSize),
+ qtd_faltam_colunas=rep(NA, MaxSize),
+ stringsAsFactors = F
+ )
+
 
 cont = 1
 
-for (ano in seq(1994,2016,2)) {
+# ===============================================================
+# Geração de nomes de arquivos
+# ===============================================================
+filenames = character(MaxSize)
+not_found_filenames = character(MaxSize/2)  # Estimo que haverá poucos inexistentes
+i = 1
+j = 1
+
+for (ano in ANOS) {
   for (uf in UFs) {
     s_ano = as.character(ano)
     filename = paste0("~/data/TSE/Resultados/", s_ano, "/", "detalhe_votacao_secao_", s_ano, "_", uf, ".txt")
-    if (!file.exists(filename)) {
-      print(paste("Arquivo '", filename, "' não encontrado"))
-      next
+    if (file.exists(filename)) {
+      filenames[i] = filename
+      i = i + 1
+    } 
+    else
+    {
+      not_found_filenames[j] = filename
+      j = j+1
+      # print(paste("Arquivo '", filename, "' não encontrado"))
+      # next
     }
+  }
+}
+
+# ===============================================================
+# Carregar os arquivos
+# ===============================================================
+
+for (filename in filenames[filenames!=""]) {
+  # break
     all = FALSE
     full=FALSE 
-    cargo=3
+    # cargo=3
+    cargos = c(3, 9)  # Vereador e prefeito
     dv = read.csv(filename, header = F, encoding = "ISO-8859", sep = ";")
     orig_lines = nrow(dv)
     names(dv) = c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO", "NUM_TURNO", "DESCRICAO_ELEICAO", "SIGLA_UF", "SIGLA_UE", "CODIGO_MUNICIPIO", "NOME_MUNICIPIO", "NUMERO_ZONA", "NUMERO_SECAO", "CODIGO_CARGO", "DESCRICAO_CARGO", "QTD_APTOS", "QTD_COMPARECIMENTO", "QTD_ABSTENCOES", "QT_VOTOS_NOMINAIS", "QT_VOTOS_BRANCOS", "QT_VOTOS_NULOS", "QT_VOTOS_LEGENDA", "QT_VOTOS_ANULADOS_APU_SEP")
     
-    if (!all) {
-      dv = dv[dv$CODIGO_CARGO == cargo, ]  # Restringir a Governador, que tem os dois turnos. TODO: O que fazer se não tiver tido 2º turno? Presidente quase sempre tem
-    }
-    
-    
     dv$TAXA_COMPARECIMENTO = dv$QTD_COMPARECIMENTO / dv$QTD_APTOS
     dv$TAXA_ABSTENCAO = dv$QTD_ABSTENCOES / dv$QTD_APTOS
     verif_taxa = dv$TAXA_ABSTENCAO + dv$TAXA_COMPARECIMENTO
-    if (any(verif_taxa != 1)) { 
-      head(dv[verif_taxa != 1, ])
-      print(paste("Inconsistênca entre quantidades de abstenções e de comparecimentos em ", unique(dv$ANO_ELEICAO), " ", unique(dv$SIGLA_UF), "\n"))
-    }
-    
+    # if (any(verif_taxa != 1)) { 
+    #  head(dv[verif_taxa != 1, ])
+    #  print(paste("Inconsistênca entre quantidades de abstenções e de comparecimentos em ", unique(dv$ANO_ELEICAO), " ", unique(dv$SIGLA_UF), "\n"))
+    #}
     
     Any_NA = any(is.na(dv))
+    # Faltam_colunas = any(is.na(dv$QT_VOTOS_ANULADOS_APU_SEP))
+    qtd_faltam_colunas = length(dv[is.na(dv$QT_VOTOS_ANULADOS_APU_SEP), ])
     
     ttt[cont,] = list(ano, uf, Any_NA,orig_lines,  nrow(dv), !any(verif_taxa != 1))
     cont = cont + 1
   }
-}
 
-ttt
+
+# ttt
