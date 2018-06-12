1c58799b496299ed3d49335764c51cf04b7ede0c
William Peixoto <william@Tanis.nowhere>
2018-06-11 21:37:00 -0300

    Ajustes em várias funções. Veja o corpo desta mensagem para maiores detalhes
    - dnormC ganhou parâmetro para subtítulo
    - nova função ErrP que calcula o Erro Padrão de uma amostra.
    	- Tem um parâmetro sigma, para passagem do desvio padrão populacional, se for conhecido
    - nova dnormCC, que usa o Erro Padrão no lugar de sd()
    - nova função cosmética tit(), que formata um título padrão com base nos dados de um data frame
    	- Essa função me ajudou a descobrir sujeira nos dados do DF, contaminados com os de GO
    	- TODO: Limpar os dados com maior vigor
    - comparaTaxaA ganhou parâmetro para título, cujo valor padrão se baseia na função tit() acima
    - nova função comparaTaxaA_EP que usa o Erro Padrão no lugar de sd()
    - nova função comparaTurnosEP, já sabe o que faz...

--- abstencao_eleitoral/aux.R
+++ abstencao_eleitoral/aux.R
@@ -3,15 +3,51 @@ fmtP = function(x) {
   paste0(format(x * 100, digits=4), "%")
 }
 
-dnormC = function(x, y, main_title="Curva Normal") {  # Abrevia dnormalComp
-  dnormalComp(mean(x), sd(x), mean(y), sd(y), main_title=main_title)
+dnormC = function(x, y, main_title="Curva Normal", subtitle="Naïve") {  # Abrevia dnormalComp Naive
+  dnormalComp(mean(x), sd(x), mean(y), sd(y), main_title=main_title, subtitle = subtitle)
 }
 
-comparaTaxaA = function(x, y, turno=1) {
+ErrP = function(x, sigma=sd(x)) {sigma/sqrt(length(x))}
+
+dnormCC = function(x, y, main_title="Curva Normal (EP)", subtitle="Com Erro Padrão") {  # Abrevia dnormalComp com Erro Padrão
+  dnormalComp(media1 = mean(x), dp1=ErrP(x), media2=mean(y), dp2 = ErrP(y), main_title=main_title, subtitle = subtitle)
+}
+
+tit = function(x) {
+  uni = function(vv, title="objeto") {
+    ob = unique(vv)
+    if (length(ob) > 1) {
+      ob = paste(ob, collapse = ", ")
+      warning(paste("Mais de um ", title, " nesse DataFrame:", ob))
+      return(ob)
+    } else {
+      #return(ob[1])
+      return(ob)
+    }
+  }
+  
+  uf = uni(x$SIGLA_UF, "UF")
+  ano = uni(x$ANO_ELEICAO, "ANO")
+  turno = uni(x$NUM_TURNO, "Turno")
+  return(paste0(uf, ": ", ano, "(", turno, ")"))
+}
+
+comparaTaxaA = function(x, y, turno=1, main_title=paste("Compara", tit(x), " e ", tit(y), "<Naïve>")) {
   x_t = split(x, x$NUM_TURNO)
   y_t = split(y, y$NUM_TURNO)
-  return(dnormC(x_t[[turno]]$TAXA_ABSTENCAO, y_t[[turno]]$TAXA_ABSTENCAO))
+  #return(
+    dnormC(x_t[[turno]]$TAXA_ABSTENCAO, y_t[[turno]]$TAXA_ABSTENCAO, main_title = main_title)
+  #)
 }
+
+comparaTaxaA_EP = function(x, y, turno=1, main_title=paste("Compara", tit(x), " e ", tit(y), "<Naïve>")) {
+  x_t = split(x, x$NUM_TURNO)
+  y_t = split(y, y$NUM_TURNO)
+  return(
+    dnormCC(x_t[[turno]]$TAXA_ABSTENCAO, y_t[[turno]]$TAXA_ABSTENCAO, main_title = main_title)
+    )
+}
+
 
 comparaTurnos = function(x) {
   x_t = split(x, x$NUM_TURNO, drop = T)
@@ -19,7 +55,14 @@ comparaTurnos = function(x) {
     return(function(x) {"Não houve segundo turno"})
   }
   return(dnormC(x_1[[1]]), x_t[[2]])
-  
+}
+
+comparaTurnosEP = function(x) {
+  x_t = split(x, x$NUM_TURNO, drop = T)
+  if (length(x_t) == 1) {
+    return(function(x) {"Não houve segundo turno"})
+  }
+  return(dnormCC(x_1[[1]]), x_t[[2]])
 }
 
 spliTurno = function(x) {
