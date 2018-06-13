da8d1beadd4d41ab973eb0b8a4b1cf4224c31ee6
William Peixoto <william@Tanis.nowhere>
2018-06-13 19:26:02 -0300

    Retirada função get_dv, externalizada
    Todos os scrips devem usar a mesma versão de get_dv.

--- abstencao_eleitoral/Sanity_check_all.R
+++ abstencao_eleitoral/Sanity_check_all.R
@@ -25,39 +25,7 @@ CAMPOS_MAIS_RELEVANTES = c("ANO_ELEICAO", "NUM_TURNO", "SIGLA_UF", "SIGLA_UE",
                            "CODIGO_MUNICIPIO", "NUMERO_ZONA", "MUMERO_SECAO", "CODIGO_CARGO",
                            "QTD_VOTOS_BRANCOS", "QTD_VOTOS_NULOS", "TAXA_ABSTENCAO")
 
-
-get_dv = function(filename, all = FALSE, full=FALSE, cargos= c(3, 9))  {
-  dv = read.csv(filename, header = F, encoding = "ISO-8859", sep = ";")
-  names(dv) = c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO", "NUM_TURNO", "DESCRICAO_ELEICAO", "SIGLA_UF", "SIGLA_UE", "CODIGO_MUNICIPIO", "NOME_MUNICIPIO", "NUMERO_ZONA", "NUMERO_SECAO", "CODIGO_CARGO", "DESCRICAO_CARGO", "QTD_APTOS", "QTD_COMPARECIMENTO", "QTD_ABSTENCOES", "QT_VOTOS_NOMINAIS", "QT_VOTOS_BRANCOS", "QT_VOTOS_NULOS", "QT_VOTOS_LEGENDA", "QT_VOTOS_ANULADOS_APU_SEP")
-  
-  if (!all) {
-    dv = dv[dv$CODIGO_CARGO %in% cargos, ]  # Restringir a Governador e prefeito, que tem os dois turnos. TODO: O que fazer se não tiver tido 2º turno? Presidente quase sempre tem. Prefeito parece ser o contrário
-  }
-  
-  
-  dv$TAXA_COMPARECIMENTO = dv$QTD_COMPARECIMENTO / dv$QTD_APTOS
-  dv$TAXA_ABSTENCAO = dv$QTD_ABSTENCOES / dv$QTD_APTOS
-  verif_taxa = dv$TAXA_ABSTENCAO + dv$TAXA_COMPARECIMENTO
-  if (any(verif_taxa != 1)) { 
-    head(dv[verif_taxa != 1, ])
-    warning(paste("Inconsistênca entre quantidades de abstenções e de comparecimentos em ", unique(dv$ANO_ELEICAO), " ", unique(dv$SIGLA_UF), "\n"))
-  }
-  
-  library(stringr)
-  dv$NUMERO_SECAO = str_pad(dv$NUMERO_SECAO, 3, pad="0") 
-  dv$NUM_ZONECAO = paste0(dv$NUMERO_ZONA, dv$NUMERO_SECAO)  
-  
-  if (!full) {  # Excluir campos desnecessários
-    # dv$QTD_COMPARECIMENTO = NULL
-    # dv$QTD_APTOS = NULL
-    # dv$DESCRICAO_CARGO = NULL
-    dv = dv[, c("ANO_ELEICAO", "NUM_TURNO", "SIGLA_UF", "SIGLA_UE", "CODIGO_MUNICIPIO", "NOME_MUNICIPIO", "NUMERO_ZONA", "NUMERO_SECAO", "CODIGO_CARGO", "NUM_ZONECAO")]
-    
-  }
-  
-  return(dv)
-}
-
+source("get_dv.R")
 # ===============================================================
 # AQUI
 # ===============================================================
