2118dd0340f31954075f43c30d219cd87e06faae
William Peixoto <william@Tanis.nowhere>
2018-06-17 12:03:11 -0300

    Esboço de script para transformar dados originais em formato conveniente para a pesquisa

---
+++ abstencao_eleitoral/trata_dados_originais.R
@@ -0,0 +1,13 @@
+# Geração de novos arquivos com os dados tratados (ou um só)
+
+# No caso deste estudo, vai coletar os dados originais de resultados de eleições no DF,
+# corrigir problemas detectados, remover colunas não utilizadas 
+# e salvar os dados em um único arquivo .csv.
+
+# ANO_ELEICAO, NUM_TURNO, NUMERO_ZONA, NUMERO_SECAO, QTD_APTOS, TAXA_ABSTENCAO, 
+# QT_VOTOS_BRANCOS, QT_VOTOS_NULOS
+
+source("get_filenames.R")
+
+
+print("Ainda não implementado")
